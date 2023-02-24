d06b677a64e4c76e09f7c0d3804328ecc61f9d25
julie thomas <juliethomas1008@gmail.com>
2023-02-24 11:03:50 +0100

    change CSS

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 6 }
-  validates :movie, uniqueness: { scope: :list }
+  validates :movie, uniqueness: { scope: :list, message: 'This movie is already on your list' }
 end
